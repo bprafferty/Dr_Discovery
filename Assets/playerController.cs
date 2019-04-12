da643d100dc3458cc41d6c4c34cf755d3a0faa4e
bprafferty03 <bprafferty03@gmail.com>
2019-04-12 13:39:12 -0700

    Created Prefabs for Player/Ground and added basic movement script

---
+++ Assets/playerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+    public int movementSpeed = 10;
+    private float moveX;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        movePlayer();
+    }
+
+    void movePlayer() {
+
+        //controls
+        moveX = Input.GetAxis("Horizontal");
+
+        //animations
+
+        //player direction
+        if (moveX < 0.0f){
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (moveX > 0.0f) {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        //physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * movementSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+}

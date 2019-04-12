64929775973eff7657ee867b63196dafef752ba0
bprafferty03 <bprafferty03@gmail.com>
2019-04-12 13:57:54 -0700

    added jumping to movement script

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class playerController : MonoBehaviour {
     public int movementSpeed = 10;
     private float moveX;
+    public int jumpPower = 1100;
+    public bool isGrounded;
+    public float distanceToPlayerFeet = 0.55f;
 
 
     // Start is called before the first frame update
@@ -15,11 +18,15 @@ public class playerController : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         movePlayer();
+        rayCast();
     }
 
     void movePlayer() {
 
         //controls
+        if (Input.GetButtonDown("Jump") && isGrounded == true) {
+            jumpPlayer();
+        }
         moveX = Input.GetAxis("Horizontal");
 
         //animations
@@ -34,5 +41,18 @@ public class playerController : MonoBehaviour {
 
         //physics
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * movementSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void jumpPlayer() {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpPower);
+        isGrounded = false;
+    }
+
+    void rayCast() {
+        RaycastHit2D downward = Physics2D.Raycast(transform.position, Vector2.down);
+        if (downward != null && downward.collider != null && downward.distance < distanceToPlayerFeet && downward.collider.tag != "Enemy") {
+            isGrounded = true;
+        }
+
     }
 }

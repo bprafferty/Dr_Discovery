3cced3719a5d5b4d63b203c4c48bca65ea7c93c2
bprafferty03 <bprafferty03@gmail.com>
2019-04-12 23:01:53 -0700

    fixed issues with enemy controller

--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -7,42 +7,61 @@ using UnityEngine.SceneManagement;
 public class enemyController : MonoBehaviour {
     public int lives = 3;
     public GameObject livesCanvas;
+    public GameObject chompCanvas;
     public int movementSpeed = 3;
     public int moveX = 1;
     public int currentLives;
 
     // Start is called before the first frame update
     void Start() {
-        
+
     }
 
     // Update is called once per frame
     void Update() {
+        chompCanvas.GetComponent<Text>().enabled = false;
         livesCanvas.gameObject.GetComponent<Text>().text = ("Lives: " + lives);
         RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(moveX, 0));
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX, 0) * movementSpeed;
-        if (hit.distance < 0.9f && moveX < 0) {
+
+        if (hit.distance < 0.9f && moveX < 0)
+        {
             moveX = 1;
             GetComponent<SpriteRenderer>().flipX = true;
-            if (hit.collider.tag == "Player") {
+            if (hit.collider.tag == "Player")
+            {
                 damage();
             }
         }
-        else if (hit.distance < 0.9f && moveX > 0) {
+        else if (hit.distance < 0.9f && moveX > 0)
+        {
             moveX = -1;
             GetComponent<SpriteRenderer>().flipX = false;
-            if (hit.collider.tag == "Player") {
+            if (hit.collider.tag == "Player")
+            {
                 damage();
             }
         }
+
     }
 
     void damage() {
         lives -= 1;
-
+        damageCanvas();
         if (lives == 0) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 
     }
+
+    IEnumerator damageCanvas() {
+        chompCanvas.GetComponent<Text>().enabled = true;
+        yield return StartCoroutine(turnOff(2.0f));
+    }
+
+    IEnumerator turnOff(float waitTime) {
+        chompCanvas.GetComponent<Text>().enabled = false;
+        yield return 0;
+    }
+
 }

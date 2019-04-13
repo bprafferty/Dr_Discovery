4b5b7e7849c6f5dfb0e9367f9872cba3d7f96c33
bprafferty03 <bprafferty03@gmail.com>
2019-04-12 20:32:22 -0700

    reconfigured the way enemies hurt player

--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class enemyController : MonoBehaviour {
     public int movementSpeed = 3;
     public int moveX = 1;
+    public int lives = 3;
+    public GameObject livesCanvas;
 
     // Start is called before the first frame update
     void Start() {
@@ -14,25 +17,30 @@ public class enemyController : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        livesCanvas.gameObject.GetComponent<Text>().text = ("Lives: " + lives);
         RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(moveX, 0));
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX, 0) * movementSpeed;
         if (hit.distance < 0.9f && moveX < 0) {
             moveX = 1;
             GetComponent<SpriteRenderer>().flipX = true;
             if (hit.collider.tag == "Player") {
-                chomp();
+                damage();
             }
         }
         else if (hit.distance < 0.9f && moveX > 0) {
             moveX = -1;
             GetComponent<SpriteRenderer>().flipX = false;
             if (hit.collider.tag == "Player") {
-                chomp();
+                damage();
             }
         }
     }
 
-    void chomp() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    void damage() {
+        lives -= 1;
+        if (lives == 0) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
     }
 }

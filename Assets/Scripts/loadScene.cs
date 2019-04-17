57e7b0af8475d7e8a0e9c094405d62357af855c1
bprafferty03 <bprafferty03@gmail.com>
2019-04-16 21:45:38 -0700

    finished menu

---
+++ Assets/Scripts/loadScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadScene : MonoBehaviour {
+    
+    public void load(string sceneName) {
+        SceneManager.LoadScene("level1");
+    }
+
+}
